1951fecb00fc662ee15351390345d28fb6760583
ziyad <ziyadsfaxi@gmail.com>
2020-05-01 19:22:33 +0800

    fix inspectionSlot validator

--- src/validators/inspectionSlot.validator.ts
+++ src/validators/inspectionSlot.validator.ts
@@ -3,10 +3,6 @@ import { Schema } from "express-validator";
 export const create: Schema = {
     from: {
         in: ["body"],
-        isInt: true,
-    },
-    test: {
-        in: ["body"],
         isString: true,
     },
 };
